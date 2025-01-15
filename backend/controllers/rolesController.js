f961f6cfcc087725f844c201480301eda35a3c50
Cyryl John Navarro <110725623+TheFreeloader@users.noreply.github.com>
2025-01-15 20:20:37 +0800

    Refactor database queries in contacts and zones controllers to remove promise wrapper for improved readability

--- backend/controllers/rolesController.js
+++ backend/controllers/rolesController.js
@@ -1,135 +1,110 @@
 const db = require('../database');
 
-//Add new role
+// Add new role
 exports.addRole = async (req, res) => {
-    try {
-        const { role_code, role_name } = req.body;
-    
-      //Require input across all fields
-      if (!role_code || !role_name) {
-        return res.status(400).json({ error: 'Invalid input', message: 'Please provide role_code and role_name' });
-      }
-    
-        const insertUserQuery = 'INSERT INTO roles (role_code, role_name) VALUES (?, ?)';
-        await db.promise().execute(insertUserQuery, [role_code, role_name]);
-    
-        res.status(201).json({ message: 'Role added successfully!' });
-      } catch (error) {
-        console.error('Error adding role', error);
-    
-        //Avoid duplicate role
-        if (error.code === 'ER_DUP_ENTRY') {
-          return res.status(409).json({ error: 'Role already exists', message: 'Please choose a different role' });
-        }
-    
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
-}; 
+  try {
+    const { role_code, role_name } = req.body;
 
-//Get all roles
-exports.getRoles = async (req, res) => {
-    
-    try{
-
-        db.query('SELECT * FROM roles', (err, result) => {
-            if (err) {
-                console.error('Error fetching items:', err);
-                res.status(500).json({ message: 'Internal Server Error '});
-            } else {
-                res.status(200).json(result);
-            }
-        });   
-
-    } catch (error) {
-
-        console.error('Error loading user:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-
+    // Require input across all fields
+    if (!role_code || !role_name) {
+      return res.status(400).json({ error: 'Invalid input', message: 'Please provide role_code and role_name' });
     }
+
+    const insertUserQuery = 'INSERT INTO roles (role_code, role_name) VALUES (?, ?)';
+    await db.execute(insertUserQuery, [role_code, role_name]);
+
+    res.status(201).json({ message: 'Role added successfully!' });
+  } catch (error) {
+    console.error('Error adding role', error);
+
+    // Avoid duplicate role
+    if (error.code === 'ER_DUP_ENTRY') {
+      return res.status(409).json({ error: 'Role already exists', message: 'Please choose a different role' });
+    }
+
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
-//Get single role
+// Get all roles
+exports.getRoles = async (req, res) => {
+  try {
+    const getAllRolesQuery = 'SELECT * FROM roles';
+    const [roles] = await db.execute(getAllRolesQuery);
+
+    res.status(200).json(roles);
+  } catch (error) {
+    console.error('Error fetching roles:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Get single role
 exports.getRole = async (req, res) => {
-    let role_id = req.params.id;
+  const role_id = req.params.id;
+
+  if (!role_id) {
+    return res.status(400).send({ error: true, message: 'Please provide role_id' });
+  }
+
+  try {
+    const getRoleQuery = 'SELECT * FROM roles WHERE id = ? AND user_id = ?';
+    const [role] = await db.execute(getRoleQuery, [role_id]);
+
+    if (role.length === 0) {
+      return res.status(404).json({ error: true, message: 'Role does not exist' });
+    }
+
+    res.status(200).json(role[0]);
+  } catch (error) {
+    console.error('Error fetching role:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Update role
+exports.updateRole = async (req, res) => {
+  try {
+    const role_id = req.params.id;
+    const { role_code, role_name } = req.body;
+
+    if (!role_code || !role_name) {
+      return res.status(400).json({ error: 'Invalid input', message: 'Please provide role code and role name' });
+    }
+
+    const updateQuery = 'UPDATE roles SET role_code = ?, role_name = ? WHERE id = ?';
+    const [result] = await db.execute(updateQuery, [role_code, role_name, role_id]);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: true, message: 'Role does not exist' });
+    }
+
+    res.status(200).json({ message: 'Role updated successfully' });
+  } catch (error) {
+    console.error('Error updating role:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Delete role
+exports.deleteRole = async (req, res) => {
+  try {
+    const role_id = req.params.id;
 
     if (!role_id) {
-        return res.status(400).send({ error: true, message: 'Please provide role_id' });
+      return res.status(400).json({ error: true, message: 'Please provide role_id' });
     }
 
-    try{
-        db.query('SELECT * FROM roles WHERE id = ?', role_id, (err, result) => {
-            if (err){
-                console.error('Error fetching items:', err);
-                res.status(500).json({ message: 'Internal Server Error' });
-            } else {
-                if (result.length === 0) {
-                    // No role found with the provided ID
-                    return res.status(404).json({ error: true, message: 'Role does not exist' });
-                }
-                res.status(200).json(result);
-            }
-        });
-    } catch (error) {
-        console.error('Error loading role:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    const deleteQuery = 'DELETE FROM roles WHERE id = ?';
+    const [result] = await db.execute(deleteQuery, [role_id]);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: true, message: 'Role not found' });
     }
+
+    res.status(200).json({ message: 'Role deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting role:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
-
-//Update Role
-exports.updateRole = async (req, res) => {
-    try {
-        const role_id = req.params.id;
-        const { role_code, role_name } = req.body;
-  
-        if (!role_code || !role_name) {
-            return res.status(400).json({ error: 'Invalid input', message: 'Please provide role code and role name' });
-        }
-  
-        const updateQuery = 'UPDATE roles SET role_code = ?, role_name = ? WHERE id = ?';
-        db.query(updateQuery, [role_code, role_name, role_id], (err, result, fields) => {
-            if (err) {
-                console.error(`Error updating role ${role_id}:`, err);
-                return res.status(500).json({ message: 'Internal Server Error' });
-            } else {
-                if (result.affectedRows === 0) {
-                    // No role found with the provided ID
-                    return res.status(404).json({ error: true, message: 'role does not exist' });
-                }
-                return res.status(200).json(result);
-            }
-        });
-    } catch (error) {
-        console.error('Error updating role:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
-
-//Delete Role
-exports.deleteRole = async (req, res) => {
-    try {
-        const role_id = req.params.id;
-  
-        if (!role_id) {
-            return res.status(400).json({ error: true, message: 'Please provide role_id' });
-        }
-  
-        const deleteQuery = 'DELETE FROM roles WHERE id = ?';
-        db.query(deleteQuery, role_id, (err, result, fields) => {
-            if (err) {
-                console.error('Error deleting role:', err);
-                res.status(500).json({ message: 'Internal Server Error' });
-            } else {
-                if (result.affectedRows > 0) {
-                    // Check if any rows were affected (indicating successful deletion)
-                    res.status(200).json({ message: 'Role deleted successfully' });
-                } else {
-                    // No rows affected means user with specified ID was not found
-                    res.status(404).json({ error: true, message: 'Role not found' });
-                }
-            }
-        });
-    } catch (error) {
-        console.error('Error deleting role:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,109 +1,98 @@
 const db = require('../database');
 const bcrypt = require('bcrypt');
 
-//Get all users
+// Get all users
 exports.getUsers = async (req, res) => {
-    try {
-    db.query('SELECT u.id, u.name, u.username, u.role_id, r.role_name, num, email FROM users u JOIN roles r ON u.role_id = r.id;', (err,result) => {
-      if (err) {
-        console.error('Error fetching items:', err);
-        res.status(500).json({ message: 'Internal Server Error '});
-      } else {
-        console.log(res)
-        res.status(200).json(result);
-      }
-    });
-
+  try {
+    const getAllUsersQuery = `
+      SELECT u.id, u.name, u.username, u.role_id, r.role_name, num, email 
+      FROM users u 
+      JOIN roles r ON u.role_id = r.id;
+    `;
+    const [users] = await db.execute(getAllUsersQuery);
+    res.status(200).json(users);
   } catch (error) {
-    
-    console.error('Error loading user:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error fetching users:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
-//Get single user
+// Get single user
 exports.getUser = async (req, res) => {
-  let user_id = req.params.id;
+  const user_id = req.params.id;
 
   if (!user_id) {
-      return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    return res.status(400).send({ error: true, message: 'Please provide user_id' });
   }
 
   try {
-      db.query('SELECT u.id, u.name, u.username, u.role_id, r.role_name, num, email FROM users u JOIN roles r ON u.role_id = r.id WHERE u.id = ?', user_id, (err, result) => {
-          if (err) {
-              console.error('Error fetching items:', err);
-              res.status(500).json({ message: 'Internal Server Error' });
-          } else {
-              if (result.length === 0) {
-                  // No user found with the provided ID
-                  return res.status(404).json({ error: true, message: 'Account does not exist' });
-              }
-              res.status(200).json(result);
-          }
-      });
+    const getUserQuery = `
+      SELECT u.id, u.name, u.username, u.role_id, r.role_name, num, email 
+      FROM users u 
+      JOIN roles r ON u.role_id = r.id 
+      WHERE u.id = ?;
+    `;
+    const [user] = await db.execute(getUserQuery, [user_id]);
+
+    if (user.length === 0) {
+      return res.status(404).json({ error: true, message: 'Account does not exist' });
+    }
+
+    res.status(200).json(user[0]);
   } catch (error) {
-      console.error('Error loading user:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error fetching user:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
-//Update User
+// Update user
 exports.updateUser = async (req, res) => {
   try {
-      const user_id = req.params.id;
-      const { name, username, password, role_id, num, email } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 10);
+    const user_id = req.params.id;
+    const { name, username, password, role_id, num, email } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      if (!user_id || !name || !username || !password || !role_id) {
-          return res.status(400).json({ error: 'Invalid input', message: 'Please provide name, username, password, role_id, num, and email' });
-      }
+    if (!user_id || !name || !username || !password || !role_id) {
+      return res.status(400).json({ error: 'Invalid input', message: 'Please provide name, username, password, role_id, num, and email' });
+    }
 
-      const updateQuery = 'UPDATE users SET name = ?, username = ?, password = ?, role_id = ?, num = ?, email = ? WHERE id = ?';
-      db.query(updateQuery, [name, username, hashedPassword, role_id, num, email, user_id], (err, result, fields) => {
-          if (err) {
-              console.error(`Error updating user ${user_id}:`, err);
-              return res.status(500).json({ message: 'Internal Server Error' });
-          } else {
-              if (result.affectedRows === 0) {
-                  // No user found with the provided ID
-                  return res.status(404).json({ error: true, message: 'Account does not exist' });
-              }
-              return res.status(200).json(result);
-          }
-      });
+    const updateQuery = `
+      UPDATE users 
+      SET name = ?, username = ?, password = ?, role_id = ?, num = ?, email = ? 
+      WHERE id = ?;
+    `;
+    const [result] = await db.execute(updateQuery, [name, username, hashedPassword, role_id, num, email, user_id]);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: true, message: 'Account does not exist' });
+    }
+
+    res.status(200).json({ message: 'User updated successfully' });
   } catch (error) {
-      console.error('Error updating user:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error updating user:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
-//Delete User
+// Delete user
 exports.deleteUser = async (req, res) => {
   try {
-      const user_id = req.params.id;
+    const user_id = req.params.id;
 
-      if (!user_id) {
-          return res.status(400).json({ error: true, message: 'Please provide user_id' });
-      }
+    if (!user_id) {
+      return res.status(400).json({ error: true, message: 'Please provide user_id' });
+    }
 
-      const deleteQuery = 'DELETE FROM users WHERE id = ?';
-      db.query(deleteQuery, user_id, (err, result, fields) => {
-          if (err) {
-              console.error('Error deleting user:', err);
-              res.status(500).json({ message: 'Internal Server Error' });
-          } else {
-              if (result.affectedRows > 0) {
-                  // Check if any rows were affected (indicating successful deletion)
-                  res.status(200).json({ message: 'User deleted successfully' });
-              } else {
-                  // No rows affected means user with specified ID was not found
-                  res.status(404).json({ error: true, message: 'User not found' });
-              }
-          }
-      });
+    const deleteQuery = 'DELETE FROM users WHERE id = ?';
+    const [result] = await db.execute(deleteQuery, [user_id]);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: true, message: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User deleted successfully' });
   } catch (error) {
-      console.error('Error deleting user:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error deleting user:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
