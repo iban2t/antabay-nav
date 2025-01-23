1565b85674052cc50dea02a1ed38a282c2077c97
iban2t <joshuaivanvillanueva@gmail.com>
2025-01-23 13:51:19 +0800

    Implemented Scrollview

--- mobile/Distress.js
+++ mobile/Distress.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Alert } from 'react-native';
 import { DataTable } from 'react-native-paper'; // Import DataTable from react-native-paper
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import config from './config'
-
+import config from './config';
 
 function Distress() {
   const [data, setData] = useState([]);
@@ -15,41 +14,37 @@ function Distress() {
       try {
         const response = await axios.get(`${config.API_BASE_URL}/nav/distress`, {
           headers: {
-            Authorization: token
-          }
+            Authorization: token,
+          },
         });
         setData(response.data);
       } catch (error) {
         console.error(error);
+        Alert.alert('Error', 'Failed to fetch distress data.');
       }
     };
     fetchData();
   }, []);
 
-  const renderRow = ({ item }) => (
-    <DataTable.Row>
-      <DataTable.Cell>{item.id}</DataTable.Cell>
-      <DataTable.Cell>{item.description}</DataTable.Cell>
-      <DataTable.Cell>{item.distress_at}</DataTable.Cell>
-    </DataTable.Row>
-  );
-
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Distress</Text>
-
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title>ID</DataTable.Title>
-          <DataTable.Title>Description</DataTable.Title>
-          <DataTable.Title>Date/Time</DataTable.Title>
-        </DataTable.Header>
-        <FlatList
-          data={data}
-          renderItem={renderRow}
-          keyExtractor={(item) => item.id.toString()}
-        />
-      </DataTable>
+      <ScrollView>
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title>ID</DataTable.Title>
+            <DataTable.Title>Description</DataTable.Title>
+            <DataTable.Title>Date/Time</DataTable.Title>
+          </DataTable.Header>
+          {data.map((item) => (
+            <DataTable.Row key={item.id}>
+              <DataTable.Cell>{item.id}</DataTable.Cell>
+              <DataTable.Cell>{item.description}</DataTable.Cell>
+              <DataTable.Cell>{item.distress_at}</DataTable.Cell>
+            </DataTable.Row>
+          ))}
+        </DataTable>
+      </ScrollView>
     </View>
   );
 }
@@ -59,14 +54,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: '#f8f8f8',
-  },
-  topNavigation: {
-    marginBottom: 16,
-    // Add more styling for top navigation if needed
-  },
-  sidebar: {
-    marginBottom: 16,
-    // Add more styling for sidebar if needed
   },
   title: {
     fontSize: 24,
